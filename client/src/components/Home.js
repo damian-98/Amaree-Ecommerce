e43f14d67e9874d9956421845f9a98bcab8536ba
Damian Tomlinson <dam982010@yahoo.com>
2023-08-09 23:17:32 -0400

    I added css media queries to make website more responsive

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Carousel from "react-bootstrap/Carousel";
 
-function Home() {
+const Home = () => {
   const [index, setIndex] = useState(0);
 
   const handleSelect = (selectedIndex, e) => {
@@ -11,18 +11,16 @@ function Home() {
   return (
     <Carousel activeIndex={index} onSelect={handleSelect}>
       <Carousel.Item>
-        <div>
-          <div className="home"></div>
-        </div>
+        <div className="home"></div>
+
         <Carousel.Caption></Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <div>
-          <div className="home"></div>
-        </div>
+        <div className="home"></div>
+
         <Carousel.Caption></Carousel.Caption>
       </Carousel.Item>
     </Carousel>
   );
-}
+};
 export default Home;
